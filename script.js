13957c7b14bfb6ee5216b4771012fd444450f732
Amaury Benítez <81054837+MauRyTo@users.noreply.github.com>
2023-03-29 10:42:58 -0600

    Changes.

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-let inCellphone = new Boolean(false);
+const body = document.querySelector("body"),
+    toggle = document.querySelector("toggle_button"),
+    buttons = document.querySelector("buttons");
 
-let navegador = navigator.userAgent;
+// toggle.addEventListener("click", () => {buttons.classList.toggle("close")});
 
-if (navegador.match(/Android/i) ||
-    navigator.userAgent.match(/webOS/i) ||
-    navigator.userAgent.match(/iPhone/i) ||
-    navigator.userAgent.match(/iPad/i) ||
-    navigator.userAgent.match(/iPod/i) ||
-    navigator.userAgent.match(/BlackBerry/i) ||
-    navigator.userAgent.match(/Windows Phone/i))
-{
-    console.log("Estás usando un dispositivo móvil!!");
-    inCellphone = true;
-    console.log(inCellphone.toString());
-}
-else
-{
-    console.log("No estás usando un móvil.");
-    inCellphone = false;
-    console.log(inCellphone.toString());
-}
+// let inCellphone = new Boolean(false);
+// 
+// let navegador = navigator.userAgent;
+// 
+// if (navegador.match(/Android/i) ||
+//     navigator.userAgent.match(/webOS/i) ||
+//     navigator.userAgent.match(/iPhone/i) ||
+//     navigator.userAgent.match(/iPad/i) ||
+//     navigator.userAgent.match(/iPod/i) ||
+//     navigator.userAgent.match(/BlackBerry/i) ||
+//     navigator.userAgent.match(/Windows Phone/i))
+// {
+//     console.log("Estás usando un dispositivo móvil!!");
+//     inCellphone = true;
+//     console.log(inCellphone.toString());
+// }
+// else
+// {
+//     console.log("No estás usando un móvil.");
+//     inCellphone = false;
+//     console.log(inCellphone.toString());
+// }
