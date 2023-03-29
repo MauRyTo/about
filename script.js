63a860cce01fee1ddf54ad7ff8d7b260e9deab1a
Amaury Benítez <81054837+MauRyTo@users.noreply.github.com>
2023-03-29 01:18:06 -0600

    Changes

---
+++ script.js
@@ -0,0 +1,22 @@
+let inCellphone = new Boolean(false);
+
+let navegador = navigator.userAgent;
+
+if (navegador.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i))
+{
+    console.log("Estás usando un dispositivo móvil!!");
+    inCellphone = true;
+    console.log(inCellphone.toString());
+}
+else
+{
+    console.log("No estás usando un móvil.");
+    inCellphone = false;
+    console.log(inCellphone.toString());
+}
